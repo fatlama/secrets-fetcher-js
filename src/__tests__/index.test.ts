c4da1a59974540fdd9f9182ddfa805c67fa8c7fc
Bruce Felt <bruce.felt@gmail.com>
2019-05-31 16:26:49 +0100

    Export MockSecretsClient

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,13 @@
 import * as index from '../index'
 import { AWSSecretsClient } from '../aws-secrets-client'
+import { MockSecretsClient } from '../mock-secrets-client'
 
 describe('index', () => {
   it('exports AWSSecretsClient', () => {
     expect(index.AWSSecretsClient).toEqual(AWSSecretsClient)
+  })
+
+  it('exports MockSecretsClient', () => {
+    expect(index.MockSecretsClient).toEqual(MockSecretsClient)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './types'
 export * from './aws-secrets-client'
+export * from './mock-secrets-client'
